96e0187864de3281a0a361aa5be01e919ea57371
Massiccio1 <girardellimassimo@gmail.com>
2023-05-08 12:50:32 +0200

    retro update

---
+++ c/doppio.c
@@ -0,0 +1,4 @@
+#include "doppio.h"
+int doppio(int a){
+    return 2*a;
+}
--- c/main.c
+++ c/main.c
@@ -1,8 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "doppio.h"
+
+int func(int a, int b){
+    int c = a+b;
+    return c*3;
+}
 
 int main(){
     int i=12;
-    printf("\nexiting with %d",i);
-    return i;
+    //printf("\nexiting with %d",i);
+    int a = doppio(12);
+    return func(a,0);
 }
